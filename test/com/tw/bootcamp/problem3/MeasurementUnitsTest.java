7a1155e3d0dba25ca8edde7beefdd44df1bde942
amisharawat <amisharawat05@outlook.com>
2026-05-08 10:54:25 +0530

    checks the comparison between 5cm and 2 inches

--- test/com/tw/bootcamp/problem3/MeasurementUnitsTest.java
+++ test/com/tw/bootcamp/problem3/MeasurementUnitsTest.java
@@ -2,15 +2,36 @@ package com.tw.bootcamp.problem3;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MeasurementUnitsTest {
-    
+
     @Test
     void shouldReturnTrueForEqualityCheckOf1FeetAnd12Inches() {
-        MeasurementUnits feet = MeasurementUnits.create(1.0, 30.48);
-        MeasurementUnits inches = MeasurementUnits.create(12.0, 2.54);
-        Boolean isEqual = feet.compare(inches);
+        MeasurementUnits oneFeet = MeasurementUnits.create(1.0, 30.48);
+        MeasurementUnits twelveInches = MeasurementUnits.create(12.0, 2.54);
+        Boolean isEqual = oneFeet.compare(twelveInches);
         assertTrue(isEqual);
     }
+
+
+    @Test
+    void shouldReturnFalseForEqualityCheckOf2FeetAnd12Inches() {
+        MeasurementUnits twoFeet = MeasurementUnits.create(2.0, 30.48);
+        MeasurementUnits twelveInches = MeasurementUnits.create(12.0, 2.54);
+        Boolean isEqual = twoFeet.compare(twelveInches);
+        assertFalse(isEqual);
+    }
+
+
+    @Test
+    void shouldReturnFalseForEqualityCheckOf5CmAnd2Inches() {
+        MeasurementUnits fiveCentimeter = MeasurementUnits.create(5.0, 1.0);
+        MeasurementUnits twoInches = MeasurementUnits.create(2.0, 2.54);
+        Boolean isEqual = twoInches.compare(fiveCentimeter);
+        assertFalse(isEqual);
+    }
+
+
 }
